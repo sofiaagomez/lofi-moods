57f044e6c5fa7985cbe43e05aea20e6da3dbce4b
sofiaagomez <sofiaxgomez97@gmail.com>
2025-12-02 22:49:57 -0300

    Agregué estilos y validaciones al form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* ------------------- Dark Mode ------------------- */
+
 const darkModeBtn = document.getElementById("darkModeBtn");
 const body = document.body;
 
@@ -13,4 +15,39 @@ darkModeBtn.addEventListener("click", () => {
         icon.classList.remove("fa-sun");
         icon.classList.add("fa-moon");
     }
+});
+
+
+/* ---------- Validaciones del Formulario ---------- */
+
+const form = document.getElementById("form-contacto");
+const emailInput = document.getElementById("email");
+const emailMsg = document.getElementById("email-mensaje");
+
+emailInput.addEventListener("input", function () {
+    if (emailInput.validity.valid) {
+        emailMsg.textContent = "Recibirás más información en " + emailInput.value;
+    } else {
+        emailMsg.textContent = "Ingresá un correo electrónico válido";
+    }
+});
+
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById("nombre").value.trim();
+    const emailValido = emailInput.validity.valid;
+
+    const moodsSeleccionados = document.querySelectorAll("input[name='mood[]']:checked");
+
+    if (nombre === "" || !emailValido || moodsSeleccionados.length === 0) {
+        alert("Por favor, completá los campos obligatorios.");
+        return;
+    }
+
+    alert("Formulario enviado correctamente.");
+
+    form.reset();
+    emailMsg.textContent = "";
 });
